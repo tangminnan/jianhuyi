9a94adae28304f09e21af815846730fe043ea127
zouying <349829327@qq.com>
2021-02-04 09:36:27 +0800

    ~

---
+++ jianhuyi-portal/src/main/java/com/jianhuyi/information/domain/MyGiftDO.java
@@ -0,0 +1,89 @@
+package com.jianhuyi.information.domain;
+import java.util.Date;
+
+/**
+ *  兑换记录
+ */
+public class MyGiftDO {
+     private  Long id;
+    /**
+     * 获得者id
+     */
+    private  Long userId;
+    /**
+     *  礼物id
+     */
+    private  Long giftId;
+    /**
+     * 兑换时间
+     */
+    private Date createTime;
+    /**
+     *  礼物名称
+     */
+    private String giftName;
+    /**
+     *  所需积分
+     */
+    private Integer score;
+    /**
+     *  封面主要图
+     */
+    private String coverImg;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getGiftId() {
+        return giftId;
+    }
+
+    public void setGiftId(Long giftId) {
+        this.giftId = giftId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getGiftName() {
+        return giftName;
+    }
+
+    public void setGiftName(String giftName) {
+        this.giftName = giftName;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getCoverImg() {
+        return coverImg;
+    }
+
+    public void setCoverImg(String coverImg) {
+        this.coverImg = coverImg;
+    }
+}
---
+++ jianhuyi-portal/src/main/java/com/jianhuyi/information/domain/UserDO.java
@@ -0,0 +1,113 @@
+package com.jianhuyi.information.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * 用户信息表
+ *
+ * @author wjl
+ * @email bushuo@163.com
+ * @date 2019-02-15 11:27:19
+ */
+public class UserDO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    //id
+    private Long id;
+
+    //真实姓名
+    private String name;
+    private Integer sex;
+    //出生年月
+    private Date birthday;
+    //所剩积分
+    private Integer scores;
+    //年龄
+    private Integer age;
+    //学校
+    private String school;
+    //班级
+    private String grade;
+    //最近一次的任务id
+    private Long taskId;
+
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public Integer getScores() {
+        return scores;
+    }
+
+    public void setScores(Integer scores) {
+        this.scores = scores;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+}
