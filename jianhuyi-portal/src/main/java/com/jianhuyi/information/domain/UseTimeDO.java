82eac818a75d5d469fd6d9dd5227f60f52d233e4
jiazhifang000 <jzf18235182968@163.com>
2021-02-22 10:27:26 +0800

    //

--- jianhuyi-portal/src/main/java/com/jianhuyi/information/domain/UseTimeDO.java
+++ jianhuyi-portal/src/main/java/com/jianhuyi/information/domain/UseTimeDO.java
@@ -3,114 +3,144 @@ package com.jianhuyi.information.domain;
 import java.io.Serializable;
 import java.util.Date;
 
-
 /**
  * @author wjl
  * @email bushuo@163.com
  * @date 2020-06-13 10:52:52
  */
 public class UseTimeDO implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    //ID
-    private Long id;
-    //使用时间点（每节点乘以5s等于使用时长）
-    private Date useTime;
-    //保存时间
-    private Date saveTime;
-    //用户id
-    private Long userId;
-    //用户id
-    private Long uploadId;
-    //设备id
-    private String equipmentId;
+  // ID
+  private Long id;
+  // 使用时间点（每节点乘以5s等于使用时长）
+  private Date useTime;
+  // 保存时间
+  private Date saveTime;
+  // 用户id
+  private Long userId;
+  // 用户id
+  private Long uploadId;
+  // 设备id
+  private String equipmentId;
 
-    private Integer userDurtion;
+  private Integer userDurtion;
 
+  private int runningTime;
 
+  private int effectiveTime;
 
-    public Long getUploadId() {
-        return uploadId;
-    }
+  private int coverTime;
 
-    public void setUploadId(Long uploadId) {
-        this.uploadId = uploadId;
-    }
+  private int noneffectiveTime;
 
-    public Integer getUserDurtion() {
-        return userDurtion;
-    }
+  public int getRunningTime() {
+    return runningTime;
+  }
 
-    public void setUserDurtion(Integer userDurtion) {
-        this.userDurtion = userDurtion;
-    }
+  public void setRunningTime(int runningTime) {
+    this.runningTime = runningTime;
+  }
 
-    public String getEquipmentId() {
-        return equipmentId;
-    }
+  public int getEffectiveTime() {
+    return effectiveTime;
+  }
 
-    public void setEquipmentId(String equipmentId) {
-        this.equipmentId = equipmentId;
-    }
+  public void setEffectiveTime(int effectiveTime) {
+    this.effectiveTime = effectiveTime;
+  }
 
-    public Long getUserId() {
-        return userId;
-    }
+  public int getCoverTime() {
+    return coverTime;
+  }
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+  public void setCoverTime(int coverTime) {
+    this.coverTime = coverTime;
+  }
 
-    /**
-     * 设置：ID
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public int getNoneffectiveTime() {
+    return noneffectiveTime;
+  }
 
-    /**
-     * 获取：ID
-     */
-    public Long getId() {
-        return id;
-    }
+  public void setNoneffectiveTime(int noneffectiveTime) {
+    this.noneffectiveTime = noneffectiveTime;
+  }
 
-    /**
-     * 设置：使用时间点（每节点乘以5s等于使用时长）
-     */
-    public void setUseTime(Date useTime) {
-        this.useTime = useTime;
-    }
+  public Long getUploadId() {
+    return uploadId;
+  }
 
-    /**
-     * 获取：使用时间点（每节点乘以5s等于使用时长）
-     */
-    public Date getUseTime() {
-        return useTime;
-    }
+  public void setUploadId(Long uploadId) {
+    this.uploadId = uploadId;
+  }
 
-    /**
-     * 设置：保存时间
-     */
-    public void setSaveTime(Date saveTime) {
-        this.saveTime = saveTime;
-    }
+  public Integer getUserDurtion() {
+    return userDurtion;
+  }
 
-    /**
-     * 获取：保存时间
-     */
-    public Date getSaveTime() {
-        return saveTime;
-    }
+  public void setUserDurtion(Integer userDurtion) {
+    this.userDurtion = userDurtion;
+  }
 
-    @Override
-    public String toString() {
-        return "UseTimeDO{" +
-                "id=" + id +
-                ", useTime=" + useTime +
-                ", saveTime=" + saveTime +
-                ", userId=" + userId +
-                ", equipmentId=" + equipmentId +
-                '}';
-    }
+  public String getEquipmentId() {
+    return equipmentId;
+  }
+
+  public void setEquipmentId(String equipmentId) {
+    this.equipmentId = equipmentId;
+  }
+
+  public Long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Long userId) {
+    this.userId = userId;
+  }
+
+  /** 设置：ID */
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /** 获取：ID */
+  public Long getId() {
+    return id;
+  }
+
+  /** 设置：使用时间点（每节点乘以5s等于使用时长） */
+  public void setUseTime(Date useTime) {
+    this.useTime = useTime;
+  }
+
+  /** 获取：使用时间点（每节点乘以5s等于使用时长） */
+  public Date getUseTime() {
+    return useTime;
+  }
+
+  /** 设置：保存时间 */
+  public void setSaveTime(Date saveTime) {
+    this.saveTime = saveTime;
+  }
+
+  /** 获取：保存时间 */
+  public Date getSaveTime() {
+    return saveTime;
+  }
+
+  @Override
+  public String toString() {
+    return "UseTimeDO{"
+        + "id="
+        + id
+        + ", useTime="
+        + useTime
+        + ", saveTime="
+        + saveTime
+        + ", userId="
+        + userId
+        + ", equipmentId="
+        + equipmentId
+        + '}';
+  }
 }
--- jianhuyi-portal/src/main/java/com/jianhuyi/owneruser/controller/OwnerUserController.java
+++ jianhuyi-portal/src/main/java/com/jianhuyi/owneruser/controller/OwnerUserController.java
@@ -21,103 +21,99 @@ import java.util.Map;
 @RequestMapping("/user")
 @RestController
 public class OwnerUserController extends BaseController {
-    @Autowired
-    OwnerUserService userService;
-    @Autowired
-    private BootdoConfig bootdoConfig;
+  @Autowired OwnerUserService userService;
+  @Autowired private BootdoConfig bootdoConfig;
 
-    /**
-     * 个人中心
-     *
-     * @return
-     */
-    @Log("获取用户信息")
-    @GetMapping("/userInfo")
-    Map<String, Object> user() {
-        Map<String, Object> map = new HashMap<>();
-        OwnerUserDO udo = userService.get(getUserId());
-        map.put("data", udo);
-        map.put("msg", "success");
-        map.put("code", 0);
-        return map;
+  /**
+   * 个人中心
+   *
+   * @return
+   */
+  @Log("获取用户信息")
+  @GetMapping("/userInfo")
+  Map<String, Object> user() {
+    Map<String, Object> map = new HashMap<>();
+    OwnerUserDO udo = userService.get(getUserId());
+    map.put("data", udo);
+    map.put("msg", "success");
+    map.put("code", 0);
+    return map;
+  }
+
+  /**
+   * 编辑用户信息
+   *
+   * @return
+   */
+  @Log("编辑用户信息")
+  @PostMapping("/editInfo")
+  Map<String, Object> editInfo(OwnerUserDO user) {
+    Map<String, Object> map = new HashMap<>();
+
+    OwnerUserDO userd = userService.get(getUserId());
+    if (user.getHeardUrl() != null) {
+      userd.setHeardUrl(user.getHeardUrl());
+    }
+    if (user.getNickname() != null) {
+      userd.setNickname(user.getNickname());
+    }
+    if (user.getUserId() != null) {
+      userd.setUserId(user.getUserId());
+    }
+    if (user.getSex() != null) {
+      userd.setSex(user.getSex());
+    }
+    if (user.getBirthday() != null) {
+      userd.setBirthday(user.getBirthday());
+    }
+    if (user.getPhone() != null) {
+      userd.setPhone(user.getPhone());
+    }
+    if (user.getFileImg() != null && user.getFileImg().getSize() > 0) {
+      String fileName = OBSUtils.uploadFile(user.getFileImg(), "jianhuyi/headUrl/");
+      userd.setHeardUrl(fileName);
     }
 
-
-    /**
-     * 编辑用户信息
-     *
-     * @return
-     */
-    @Log("编辑用户信息")
-    @PostMapping("/editInfo")
-    Map<String, Object> editInfo(OwnerUserDO user) {
-        Map<String, Object> map = new HashMap<>();
-
-        OwnerUserDO userd = userService.get(getUserId());
-        if (user.getHeardUrl() != null) {
-            userd.setHeardUrl(user.getHeardUrl());
-        }
-        if (user.getNickname() != null) {
-            userd.setNickname(user.getNickname());
-        }
-        if (user.getUserId() != null) {
-            userd.setUserId(user.getUserId());
-        }
-        if (user.getSex() != null) {
-            userd.setSex(user.getSex());
-        }
-        if (user.getBirthday() != null) {
-            userd.setBirthday(user.getBirthday());
-        }
-        if (user.getPhone() != null) {
-            userd.setPhone(user.getPhone());
-        }
-        if (user.getFileImg() != null && user.getFileImg().getSize() > 0) {
-            String fileName = OBSUtils.uploadFile(user.getFileImg(), "jianhuyi/headUrl/");
-            userd.setHeardUrl(fileName);
-        }
-
-        userd.setUpdateTime(new Date());
-        if (userService.update(userd) > 0) {
-            map.put("msg", "保存成功");
-            map.put("data", "");
-            map.put("code", 0);
-        } else {
-            map.put("msg", "保存失败");
-            map.put("data", "");
-            map.put("code", -1);
-        }
-
-        return map;
+    userd.setUpdateTime(new Date());
+    if (userService.update(userd) > 0) {
+      map.put("msg", "保存成功");
+      map.put("data", "");
+      map.put("code", 0);
+    } else {
+      map.put("msg", "保存失败");
+      map.put("data", "");
+      map.put("code", -1);
     }
 
-    /**
-     * 个人中心
-     *
-     * @return
-     */
-    @Log("获取用户列表")
-    @GetMapping("/userList")
-    R userList() {
-        Map<String, Object> map = new HashMap<>();
+    return map;
+  }
 
-        List<OwnerUserDO> ownerUserDOList = userService.list(null);
-        map.put("data", ownerUserDOList);
+  /**
+   * 个人中心
+   *
+   * @return
+   */
+  @Log("获取用户列表")
+  @GetMapping("/userList")
+  R userList() {
+    Map<String, Object> map = new HashMap<>();
 
-        return R.ok(map);
-    }
+    List<OwnerUserDO> ownerUserDOList = userService.list(null);
+    map.put("data", ownerUserDOList);
 
-    @Log("获取用户列表")
-    @GetMapping("/getUserByName")
-    R getUserByName(String name) {
-        Map<String, Object> map = new HashMap<>();
-        Map<String, Object> params = new HashMap<>();
-        params.put("name", name);
+    return R.ok(map);
+  }
 
-        List<OwnerUserDO> ownerUserDOList = userService.list(params);
-        map.put("data", ownerUserDOList);
+  @Log("获取用户列表")
+  @GetMapping("/getUserByName")
+  R getUserByName(String name) {
+    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> params = new HashMap<>();
+    params.put("name", name);
 
-        return R.ok(map);
-    }
+    List<OwnerUserDO> ownerUserDOList = userService.list(params);
+    map.put("data", ownerUserDOList);
 
+    return R.ok(map);
+  }
 }
