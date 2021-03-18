b084a1892305fd606eb8eb404df41b7d2bb470f1
zouying <349829327@qq.com>
2021-03-18 18:23:29 +0800

    `

--- jianhuyi-portal/src/main/java/com/jianhuyi/system/config/CorsConfig.java
+++ jianhuyi-portal/src/main/java/com/jianhuyi/system/config/CorsConfig.java
@@ -1,27 +1,9 @@
 package com.jianhuyi.system.config;
 
-import com.google.common.collect.Maps;
-import com.jianhuyi.system.filter.CorsFilter;
 
-import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * 跨域过滤拦截器
@@ -33,8 +15,8 @@ public class CorsConfig  extends WebMvcConfigurerAdapter {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedOrigins("*")
-				.allowCredentials(true)
-				.allowedMethods("GET", "POST", "DELETE", "PUT")
-				.maxAge(3600);
+				.allowedHeaders("*")
+				.allowedMethods("*")
+				.maxAge(300*1000);
 	}
 }  
